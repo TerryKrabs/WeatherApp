700e520c876d89e11ba466ad05d5fbf07b52d314
TerryKrabs <thegoldenlemon1@gmail.com>
2023-12-07 16:47:18 -0800

    day 4

--- scripts/app.js
+++ scripts/app.js
@@ -1,7 +1,13 @@
+let userSearch = document.getElementById('searchBar');
+let searchbutton = document.getElementById('searchButton')
+//Search Bar
+let userData = userSearch.value;
 // Current Temp Api
 let currenttemp = document.getElementById("CurrentTemp")
+function apicall(){
 
-fetch('https://api.openweathermap.org/data/2.5/weather?q=Stockton&appid=fc1e2458b8d43ad75dfc9b71cf4157db&units=imperial')
+
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${userData}&appid=fc1e2458b8d43ad75dfc9b71cf4157db&units&units=imperial`)
     .then((response)=>{
   
         return response.json();
@@ -25,8 +31,13 @@ fetch('https://api.openweathermap.org/data/2.5/weather?q=Stockton&appid=fc1e2458
   .then((response) =>{
     return response.json();
 })
- .then((data=> lowtemp.innerHTML = Math.floor(data.main.temp_min)))  
+ .then((data=> lowtemp.innerHTML = Math.floor(data.main.temp_min))) 
+}
 
+
+searchbutton.addEventListener('click', function (e){
+    apicall();
+})
   // Date / Time 
 let date = new Date();
 let time = new Date();
